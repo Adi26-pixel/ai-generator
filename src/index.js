c76053b4722a8965e940efda04bbfa3469ad2ead
Adeline Monsengo <59971657+Adi26-pixel@users.noreply.github.com>
2025-06-27 16:55:02 +0200

    Added basic Javascript and typewriter plugin

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function generateRecipe(event) {
+  event.preventDefault();
+
+  new Typewriter("#recipe", {
+    strings: "Recipe is here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+
+recipeFormElement.addEventListener("submit", generateRecipe);
